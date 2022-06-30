0d3fad48a6cdf2422c299c1647fad64b351c2bb6
Adekunle Bolaji <adexbolaji100@gmail.com>
2022-06-30 05:43:11 -0700

    print adumbass message

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * print 'with proper grammer,
+ * but the outcome is a piece of art
+ */
+
 int main()
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");

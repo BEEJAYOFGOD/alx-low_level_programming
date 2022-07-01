1b1ec565e287abf5a8add5779d4c90beca620a92
Adekunle Bolaji <adexbolaji100@gmail.com>
2022-06-30 18:42:58 -0700

    A c program that prints Programming is a puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+	char str[] = "Programming is like building a multiingual puzzle.\n";
+	puts(str1);
+
+	return 0;
+
+}

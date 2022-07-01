ce6533d205257aa2a5144eb42f0930b626108b26
Adekunle Bolaji <adexbolaji100@gmail.com>
2022-06-30 19:53:20 -0700

    added

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	char chart;
+	int intg;
+	long int lintg;
+	long long int llintg;
+	float flt;
+
+	printf("Size of a char: %d bytes", sizeof(chart));
+	printf("Size of an int: %d bytes", sizeof(intg));
+	printf("Size of a long int: %d bytes", sizeof(lintg));
+	printf("Size of a long long int: %d bytes", sizeof(lintg));
+	printf("Size of a float: %d bytes", sizeof(flt);
+
+	return (0);
+}

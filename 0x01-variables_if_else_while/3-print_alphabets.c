78e8774d85df437e16f6576e73352f474628dbb9
Adekunle Bolaji <adexbolaji100@gmail.com>
2022-07-04 10:49:08 -0700

    Task 03

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+# include <stdio.h>
+
+
+/**
+* main - print the alphabets in lowercase
+* Return: Always 0  (Success)
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}

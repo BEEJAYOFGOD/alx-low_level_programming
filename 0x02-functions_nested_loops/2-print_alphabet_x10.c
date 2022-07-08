0293b0a877c6cd8eea52aaf7f55c6c02a2567f60
Adekunle Bolaji <adexbolaji100@gmail.com>
2022-07-08 11:53:56 -0700

    2nd hot fix

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - print 10 times the alphabet, in lowercase
+*/
+
+void print_alphabet_x10(void)
+{
+	int alp, count;
+
+	for (count = 0; count <= 9; count++)
+		for (alp = 'a'; alp <= 'z'; alp++)
+			_putchar(alp);
+
+	_putchar('\n');		
+}

98083ea99274741c2b201e66d906a517f03b4872
Adekunle Bolaji <adexbolaji100@gmail.com>
2022-07-08 12:05:07 -0700

    2nd hot fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,11 @@ void print_alphabet_x10(void)
 	int alp, count;
 
 	for (count = 0; count <= 9; count++)
+	{
 		for (alp = 'a'; alp <= 'z'; alp++)
+		{
 			_putchar(alp);
-		_putchar(\n);
-
-		
+		}
+		_putchar('\n');
+	}
 }

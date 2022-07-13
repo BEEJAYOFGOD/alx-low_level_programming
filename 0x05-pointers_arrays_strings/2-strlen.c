3e4d1dda5bff5d738bede94ef7e324f978d6a15b
Adekunle Bolaji <adexbolaji100@gmail.com>
2022-07-13 16:16:33 -0700

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strlen - return the length of a string
+* Return: int
+* @s: string
+*/
+
+
+int _strlen(char *s)
+{
+	int n;
+
+	for (n = 0; *s != '\0' s++)
+	{
+		n++;
+	}
+
+	return (n);
+}

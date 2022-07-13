ca0b1762f5640d65917ddb89b48bda8a318bea8f
Adekunle Bolaji <adexbolaji100@gmail.com>
2022-07-13 16:39:41 -0700

    print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _puts - prints out  a string literal
+* @str: string
+*/
+
+void _puts(char *str)
+{
+	int n;
+
+	for (n = 0; *s != '\0'; s++)
+	{
+		putchar(s[n]);
+		n++;
+	}
+}

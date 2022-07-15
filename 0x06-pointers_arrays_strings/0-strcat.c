92215ad4ca79d61e8d0cf519fe3663fa5caec2ee
Adekunle Bolaji <adexbolaji100@gmail.com>
2022-07-14 19:17:47 -0700

    dome

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,22 @@
 #include "main.h"
-
 /**
- * *_memset - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: char to copy
- * @n: number of times to copy b
- *
- * Return: pointer to the memory area s
- */
-char *_memset(char *s, char b, unsigned int n)
+ * *_strcat - concatenates two strings
+ * @dest: pointer destination
+ * @src: pointer source
+ * Return: void
+*/
+
+char *_strcat(char *dest, char *src)
 {
-	unsigned int i;
+int a = -1, i;
+for (i = 0; dest[i] != '\0'; i++)
+;
 
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
+do {
+	a++;
+	dest[i] = src[a];
+	i++;
+} while (src[a] != '\0');
 
-	return (s);
+return (dest);
 }
